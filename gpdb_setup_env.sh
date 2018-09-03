d3d23d0b44d8f59b0fed8205e241619de7c5e238
Peifeng Qiu <pqiu@pivotal.io>
2018-09-03 17:40:02 +0800

    set os parameter on segs

--- gpdb_setup_env.sh
+++ gpdb_setup_env.sh
@@ -3,3 +3,4 @@ mkfs.ext4 /dev/nvme0n1
 mkdir /data
 mount /dev/nvme0n1 /data
 chown gpadmin:gpadmin /data
+echo "500 1024000 200 4096" > /proc/sys/kernel/sem
--- gpdb_setup_remote.sh
+++ gpdb_setup_remote.sh
@@ -7,11 +7,11 @@ tar -xf gpss_component.tar.gz -C greenplum-db-devel
 sed -i -e "s/^GPHOME=.*/GPHOME=\/home\/gpadmin\/greenplum-db-devel/g" greenplum-db-devel/greenplum_path.sh
 source greenplum-db-devel/greenplum_path.sh
 
+gpscp -f hosts_segs gpdb_setup_env.sh =:/home/gpadmin/
+echo "sudo ./gpdb_setup_env.sh" | gpssh -f hosts_segs
+
 gpssh-exkeys -f hosts_all
 gpseginstall -f hosts_all
-
-gpscp -f hosts_segs gpdb_setup_env.sh =:/home/gpadmin/
-echo "sudo gpdb_setup_env.sh" | gpssh -f hosts_segs
 
 mkdir -p $MASTER_BASE_DIR
 echo "mkdir -p $SEGMENT_BASE_DIR" | gpssh -f hosts_segs
